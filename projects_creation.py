377c5bc95c313ef6a67e3361fa509a1f2bd9aca5
DimitarManolovv <108031142+DimitarManolovv@users.noreply.github.com>
2023-03-17 15:19:22 +0200

    Add files via upload

---
+++ projects_creation.py
@@ -0,0 +1,4 @@
+ime_na_arhitekt= input()
+broi_na_proekti= int(input())
+nujni_chasove_za_proekt= broi_na_proekti * 3
+print(f"The architect {ime_na_arhitekt} will need {nujni_chasove_za_proekt} hours to complete {broi_na_proekti} project/s.")
