18b918d4f31a7737daf09d118a5abaec3f4b37bc
DimitarManolovv <108031142+DimitarManolovv@users.noreply.github.com>
2023-03-30 20:54:33 +0300

    Add files via upload

---
+++ day_of_the_week.py
@@ -0,0 +1,19 @@
+chislo = int(input())
+
+if chislo <= 7:
+    if chislo == 1:
+        print('Monday')
+    if chislo == 2:
+        print('Tuesday')
+    if chislo == 3:
+        print('Wednesday')
+    if chislo == 4:
+        print('Thursday')
+    if chislo == 5:
+        print('Friday')
+    if chislo == 6:
+        print('Saturday')
+    if chislo == 7:
+        print('Sunday')
+else:
+    print('Error')
